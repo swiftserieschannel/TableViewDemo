31e67c3f3fa6e3a19ce4ef19bbe555772b07cf1a
Chandra Bhushan <swiftserieschannel@gmail.com>
2020-01-25 19:58:42 +0530

    Initial Commit

---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Chandra Bhushan on 25/01/2020.
+//  Copyright © 2020 Chandra Bhushan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
